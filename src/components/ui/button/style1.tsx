180e97b55e6c03240e9aa05198f593a028606a26
mmorot <mmorot@student.42lyon.fr>
2025-12-21 21:21:29 +0100

    fix: Update ButtonStyle1 component to accept optional children prop and adjust SVG transition durations

--- src/components/ui/button/style1.tsx
+++ src/components/ui/button/style1.tsx
@@ -1,19 +1,19 @@
 import { createElement } from 'my-react';
 
-export function ButtonStyle1({ children }: { children: string }) {
+export function ButtonStyle1({ children }: { children?: any }) {
 	return (
 		<div className="group grid-area-button relative flex h-36 w-fit justify-center justify-self-center px-8">
 			<svg
 				aria-hidden="true"
 				xmlns="http://www.w3.org/2000/svg"
-				className="text-secondary absolute top-0 bottom-0 left-0 aspect-24/202 h-full transition-all duration-300 group-hover:-left-[5px]"
+				className="text-secondary absolute top-0 bottom-0 left-0 aspect-24/202 h-full transition-all duration-200 group-hover:-left-[5px]"
 			>
 				<use href="/svg/buttonSymbol.svg"></use>
 			</svg>
 			<svg
 				aria-hidden="true"
 				xmlns="http://www.w3.org/2000/svg"
-				className="text-secondary absolute top-0 right-0 bottom-0 aspect-24/202 h-full rotate-180 transition-all duration-300 group-hover:-right-[5px]"
+				className="text-secondary absolute top-0 right-0 bottom-0 aspect-24/202 h-full rotate-180 transition-all duration-200 group-hover:-right-[5px]"
 			>
 				<use href="/svg/buttonSymbol.svg"></use>
 			</svg>
